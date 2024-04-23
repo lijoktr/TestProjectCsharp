cd42666885151378674608adbe18309df9e52b6d
lijo <lijomon24@gmail.com>
2024-04-23 11:53:06 +0100

    Initial commit

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,51 @@
+
+
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+using System.Diagnostics;
+using NuGet.Frameworks;
+using TestProject_C_seleniumframework.Utilities;
+
+namespace TestProject_C_seleniumframework.Tests
+{
+    public class Tests : Base
+    {
+        
+
+        [Test]
+        public void Test1()
+        {
+
+            TestContext.Progress.WriteLine(driver.Url);
+            TestContext.Progress.WriteLine(driver.Title);
+
+
+            //driver.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");
+            driver.FindElement(By.Id("username")).SendKeys("rahulshettyacadem");
+            driver.FindElement(By.Name("password")).SendKeys("learning");
+            //driver.FindElement(By.CssSelector("input[type='checkbox'")).Click();
+            driver.FindElement(By.XPath("//div[@class='form-group']/label/span/input")).Click();
+            driver.FindElement(By.XPath("//input[@id='signInBtn']")).Click();
+
+            // to collect error message give wrong username
+            string er_message = driver.FindElement(By.ClassName("alert-danger")).Text;
+            TestContext.Progress.WriteLine(er_message);
+
+            IWebElement Link_1 = driver.FindElement(By.LinkText("Free Access to InterviewQues/ResumeAssistance/Material"));
+            String actual_attr = Link_1.GetAttribute("href");
+            String expected_attr = "https://rahulshettyacademy.com/documents-request";
+            Assert.That(actual_attr, Is.EqualTo(expected_attr));
+            //Assert.AreEqual(expected_attr, actual_attr);
+
+        }
+
+
+
+    }
+}
---
+++ Utilities/Class1.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TestProject_C_seleniumframework.Utilities
+{
+    public class Base
+    {
+        public IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        [TearDown]
+
+        public void Close()
+        {
+            Thread.Sleep(5000);
+            driver.Close();
+        }
+    }
+}
